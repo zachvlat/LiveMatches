556e5cdb83078a0ab7f34ad8acbafaaa47218021
zachvlat <zachariasvlatakis@gmail.com>
2026-07-23 15:58:18 +0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zachvlat.footballscores"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
